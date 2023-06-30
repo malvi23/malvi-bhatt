6e52717cb480a7b7d2a82af0df1ee349d9cfea14
malvi23 <malvipanchal@gmail.com>
2023-06-30 13:12:14 -0400

    Added server url to cors config

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // Enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:4200", "http://127.0.0.1:4200"],
+    origin: ["https://malvi-bhatt-server.onrender.com","http://localhost:4200", "http://127.0.0.1:4200"],
     credentials: true,
   })
 );
